f332b1f2995736f305fb35244914b190aa5383fd
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-06 14:53:48 -0700

    starting to design homescreen view

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/HomeScreenView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/HomeScreenView.swift
@@ -6,10 +6,69 @@
 //
 
 import SwiftUI
+import FluidGradient
 
 struct HomeScreenView: View {
+    @EnvironmentObject var userService: UserController
+    @State private var showingManageBills = false
+    @State private var presentationDetent = PresentationDetent.medium
+    var horizontalPadding: CGFloat = 12
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 20) {
+                HStack {
+                    Text(userService.username == nil ?
+                         "Welcome to Billsly!" : "Welcome, \(userService.username)!")
+                        .font(.title)
+                }
+                .padding(.horizontal, horizontalPadding)
+                RoundedRectangle(cornerRadius: 12)
+                    .overlay {
+                        ZStack {
+                            FluidGradient(blobs: userService.getColors(),
+                                          speed: 0.35,
+                                          blur: 0.65)
+                            
+                            Text("Test")
+                                .font(.title.bold())
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    .frame(height: 250)
+                    .padding(.horizontal, horizontalPadding)
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        
+                    }
+                }
+                
+                Spacer()
+                
+                
+            }
+            .sheet(isPresented: $showingManageBills) {
+                    ManageBillsView()
+                        .environmentObject(userService)
+                        .presentationDetents(
+                            [.medium, .large],
+                            selection: $presentationDetent
+                        )
+            }
+            .task {
+                await userService.loadDefaultCategories()
+                await userService.generateTestBills()
+            }
+        }
     }
 }
 
