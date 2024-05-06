d6f9d68d065bf7ee8dcacf0c0a1a01b0e68fa1a3
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-06 16:44:54 -0700

    Added more to the header view. changed button colors but wip

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/HomeScreenView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/HomeScreenView.swift
@@ -26,109 +26,157 @@ struct HomeScreenView: View {
     }
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 20) {
-                RoundedRectangle(cornerRadius: 12)
-                    .overlay {
-                        ZStack {
-                            FluidGradient(blobs: userService.getColors(),
-                                          speed: 0.35,
-                                          blur: 0.65)
+        VStack(spacing: 20) {
+            Rectangle()
+                .overlay {
+                    ZStack {
+                        FluidGradient(blobs: userService.getColors(),
+                                      speed: 0.25,
+                                      blur: 0.75)
+                        
+                        VStack {
+                            HStack {
+                                Text("Billsly")
+                                    .font(.title2)
+                                Spacer()
+                            }
+                            HStack {
+                                Text(userService.username == nil ? "Welcome back!" : "Welcome back, \(userService.username)!")
+                                    .font(.title2)
+                                Spacer()
+                            }
+                            Spacer()
                             
-                            Text("Test")
-                                .font(.title.bold())
-                                .foregroundStyle(.white)
+                            HStack {
+                                Rectangle()
+                                    .foregroundStyle(.background)
+                                    .opacity(0.6)
+                                    .cornerRadius(12)
+                                    .overlay {
+                                        VStack(alignment: .center, spacing: 9) {
+                                            Text("Expenses tracked this month:")
+                                                .multilineTextAlignment(.center)
+                                            Text("\(userService.userBills.count)")
+                                                .font(.title)
+                                                .fontWeight(.bold)
+                                        }
+                                    }
+                                Rectangle()
+                                    .foregroundStyle(.background)
+                                    .opacity(0.6)
+                                    .cornerRadius(12)
+                                    .overlay {
+                                        VStack(alignment: .center, spacing: 9) {
+                                            Text("Bills left to pay this month:")
+                                                .multilineTextAlignment(.center)
+                                            Text("\(userService.userBills.count)")
+                                                .font(.title)
+                                                .fontWeight(.bold)
+                                        }
+                                    }
+                            }
+                            
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Text(df.string(from: Date.now))
+                            }
                         }
+                        .padding(12)
                     }
-                    .frame(height: 220)
-                    .padding(.horizontal, horizontalPadding)
+                }
+                .foregroundStyle(.secondary)
+                .cornerRadius(12)
+                .frame(height: 220)
+                .padding(.horizontal, horizontalPadding)
+            
+            HStack {
+                Button {
+                    showingManageBills.toggle()
+                } label: {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(Color(uiColor: ColorsHelper.celadonGreen))
+                        .overlay {
+                            Text("Add new expense")
+                                .foregroundStyle(.background)
+                        }
+                        .frame(height: 50)
+                }
                 
+                Button {
+                    showingManageBills.toggle()
+                } label: {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(Color(uiColor: ColorsHelper.celadonGreen))
+                        .overlay {
+                            Text("I paid a bill")
+                                .foregroundStyle(.background)
+                        }
+                        .frame(height: 50)
+                }
+            }
+            .padding(.horizontal, horizontalPadding)
+            
+            VStack {
                 HStack {
-                    Button {
-                        showingManageBills.toggle()
-                    } label: {
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.cyan.gradient)
-                            .overlay {
-                                Text("Manage Bills")
-                            }
-                            .frame(height: 50)
-                    }
-                    
-                    
-                    Button {
-                        showingManageBills.toggle()
-                    } label: {
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.orange.gradient)
-                            .overlay {
-                                Text("Manage Bills")
-                            }
-                            .frame(height: 50)
-                    }
+                    Text("Your next few bills at a glance")
+                        .font(.subheadline)
+                        .foregroundStyle(.foreground)
+                    Spacer()
                 }
-                .padding(.horizontal, horizontalPadding)
-                
-                VStack {
+                List(userService.userBills.prefix(3), id: \.identifier) { bill in
                     HStack {
-                        Text("Your next few bills at a glance")
-                            .font(.subheadline)
-                            .foregroundStyle(.gray)
+                        VStack(alignment: .leading) {
+                            Text(bill.name)
+                            Text("Due: " + df.string(from: bill.dueByDate))
+                        }
                         Spacer()
-                        }
-                    List(userService.userBills.prefix(3), id: \.identifier) { bill in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(bill.name)
-                                Text("Due: " + df.string(from: bill.dueByDate))
-                            }
-                            Spacer()
-                            Text("\(bill.dollarAmount as NSNumber, formatter: nf)")
-                        }
+                        Text("\(bill.dollarAmount as NSNumber, formatter: nf)")
                     }
-                    .listStyle(.inset)
                 }
-                
-                .padding(.horizontal, horizontalPadding)
-                
-                VStack {
+                .listStyle(.inset)
+                .cornerRadius(12)
+            }
+            
+            .padding(.horizontal, horizontalPadding)
+            
+            VStack {
+                HStack {
+                    Text("Your next few bills at a glance")
+                        .font(.subheadline)
+                        .foregroundStyle(.foreground)
+                    Spacer()
+                }
+                List(userService.userBills.prefix(3), id: \.identifier) { bill in
                     HStack {
-                        Text("Your next few bills at a glance")
-                            .font(.subheadline)
-                            .foregroundStyle(.gray)
+                        VStack(alignment: .leading) {
+                            Text(bill.name)
+                            Text("Due: " + df.string(from: bill.dueByDate))
+                        }
                         Spacer()
-                        }
-                    List(userService.userBills.prefix(3), id: \.identifier) { bill in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(bill.name)
-                                Text("Due: " + df.string(from: bill.dueByDate))
-                            }
-                            Spacer()
-                            Text("\(bill.dollarAmount as NSNumber, formatter: nf)")
-                        }
+                        Text("\(bill.dollarAmount as NSNumber, formatter: nf)")
                     }
-                    .listStyle(.inset)
                 }
-                
-                .padding(.horizontal, horizontalPadding)
-                
-                Spacer()
-                
-                
+                .listStyle(.inset)
+                .cornerRadius(12)
             }
-            .sheet(isPresented: $showingManageBills) {
-                    ManageBillsView()
-                        .environmentObject(userService)
-                        .presentationDetents(
-                            [.medium, .large],
-                            selection: $presentationDetent
-                        )
-            }
-            .task {
-                await userService.loadDefaultCategories()
-                await userService.generateTestBills()
-            }
+            .padding(.horizontal, horizontalPadding)
+            
+            Spacer()
+            
+        }
+        .background(.quaternary)
+        .sheet(isPresented: $showingManageBills) {
+            ManageBillsView()
+                .environmentObject(userService)
+                .presentationDetents(
+                    [.medium, .large],
+                    selection: $presentationDetent
+                )
+        }
+        .task {
+            await userService.loadDefaultCategories()
+            await userService.generateTestBills()
         }
     }
 }
