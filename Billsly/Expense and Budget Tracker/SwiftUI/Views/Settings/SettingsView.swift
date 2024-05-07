a851bcf6bcbe96041276049ebf11c9aa6a5c2ea5
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-07 15:10:05 -0700

    started stubbing out and creating settings view

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Settings/SettingsView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Settings/SettingsView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var userService: UserController
+    @State private var username: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                HStack {
+                    TextField((userService.username != nil ? "Your name is \(userService.username ?? "unknown")" : "Enter your name"), text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button {
+                        userService.setUsername(username)
+                    } label : {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .overlay {
+                                Text("Save")
+                                    .foregroundStyle(Color(.label))
+                            }
+                            .frame(width: 80)
+                    }
+                }
+            } header: {
+                Text("User Information")
+            }
+            
+            Section {
+                    SettingsSupportCell(image: "hand.thumbsup",
+                                        text: "Leave a rating",
+                                        subtext: "Support my app by leaving a review!") {
+                        
+                    }
+                    SettingsSupportCell(image: "paperplane",
+                                        text: "Share the app",
+                                        subtext: "Know someone with bills? Send this app their way!") {
+                        
+                    }
+
+                    SettingsSupportCell(image: "envelope",
+                                        text: "Send feedback",
+                                        subtext: "Email thoughts, bugs, or questions.") {
+                        
+                    }
+                    
+                
+            } header: {
+                Text("Support the Developer")
+            }
+            
+            SettingsFooter()
+        }
     }
 }
 
 #Preview {
-    SettingsView()
+    @StateObject var userService = UserController()
+    
+    return SettingsView()
+        .environmentObject(userService)
 }
