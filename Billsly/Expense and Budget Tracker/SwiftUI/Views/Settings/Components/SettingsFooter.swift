6c5b629269763dae68ca9ba6ff28752527cb8fe6
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-07 22:10:53 -0700

    added app version string

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Settings/Components/SettingsFooter.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Settings/Components/SettingsFooter.swift
@@ -8,29 +8,36 @@
 import SwiftUI
 
 struct SettingsFooter: View {
+    @EnvironmentObject var settingsService: SettingsService
+    
     var body: some View {
-        HStack {
-            Spacer()
-            Text("Crafted with")
-            Image(systemName: "heart.fill")
-                .foregroundStyle(Color(uiColor: UIColor(red: 254,
-                                                        green: 95,
-                                                        blue: 85)))
-            Text("by")
-            Button("@CaptainnClayton") {
-                let twitterHandle = "CaptainnClayton"
-                let appURL = URL(string: "twitter://user?screen_name=\(twitterHandle)")!
-                let webURL = URL(string: "https://twitter.com/\(twitterHandle)")!
+        VStack(spacing: 8) {
+            Text(settingsService.appVersionString)
+                .font(.headline)
                 
-                let application = UIApplication.shared
-                
-                if application.canOpenURL(appURL as URL) {
-                    application.open(appURL)
-                } else {
-                    application.open(webURL)
+            HStack {
+                Spacer()
+                Text("Crafted with")
+                Image(systemName: "heart.fill")
+                    .foregroundStyle(Color(uiColor: UIColor(red: 254,
+                                                            green: 95,
+                                                            blue: 85)))
+                Text("by")
+                Button("@CaptainnClayton") {
+                    let twitterHandle = "CaptainnClayton"
+                    let appURL = URL(string: "twitter://user?screen_name=\(twitterHandle)")!
+                    let webURL = URL(string: "https://twitter.com/\(twitterHandle)")!
+                    
+                    let application = UIApplication.shared
+                    
+                    if application.canOpenURL(appURL as URL) {
+                        application.open(appURL)
+                    } else {
+                        application.open(webURL)
+                    }
                 }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
