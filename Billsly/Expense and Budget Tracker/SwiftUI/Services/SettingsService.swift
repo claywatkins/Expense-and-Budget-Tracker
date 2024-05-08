6882ae39aff9837701b0a05b9a46f24be8e045a9
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-07 22:10:16 -0700

    settings service has been created. might delete.

--- Billsly/Expense and Budget Tracker/SwiftUI/Services/SettingsService.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Services/SettingsService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+class SettingsService: ObservableObject {
+    
+    let appURLForSharing = URL(string: "https://apps.apple.com/us/app/billsly/id1560270556")!
+    
+    var appVersionString: String {
+        if let appVersion = UIApplication.appVersion {
+            return "Billsly App version \(appVersion)"
+        } else {
+            return ""
+        }
+    }
+}
