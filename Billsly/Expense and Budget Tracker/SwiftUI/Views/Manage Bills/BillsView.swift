2437a8e68800fe5381c73f0179219b859371e1da
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-21 09:33:27 -0700

    removed calendar in preparation of making my own

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/BillsView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/BillsView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct BillsView: View {
+    @EnvironmentObject var userService: UserController
+    @State private var date = Date()
+    @State private var expandListView = false
+    
     var body: some View {
         VStack {
+                
+            BillListSection(billList: userService.unpaidBills,
+                            expandListView: $expandListView,
+                            date: $date,
+                            sectionTitle: "Unpaid Bills")
+                .environmentObject(userService)
             
-            Spacer()
+            BillListSection(billList: userService.paidBills,
+                            expandListView: $expandListView,
+                            date: $date,
+                            sectionTitle: "Paid Bills")
+                .environmentObject(userService)
         }
         .background(.quaternary)
+        .sheet(isPresented: $expandListView, content: {
+            ManageBillsView(billList: $userService.currentList)
+                .environmentObject(userService)
+        })
     }
 }
 
 #Preview {
-    BillsView()
+    @StateObject var userService = UserController()
+    return BillsView()
+        .environmentObject(userService)
 }
+
+
