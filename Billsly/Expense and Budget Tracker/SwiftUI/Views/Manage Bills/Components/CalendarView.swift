b53cad2627782d29bbcfd498e6fe2cd35f09aa86
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-21 11:12:03 -0700

    changed a few things

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/Components/CalendarView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/Components/CalendarView.swift
@@ -8,18 +8,57 @@
 import SwiftUI
 
 struct CalendarView: View {
-    @State private var color: Color = .blue
-    @State private var date = Date.now
-    let daysOfWeek = []
+    @EnvironmentObject var settingsService: SettingsService
+    let date: Date
+    @State private var days: [Date] = []
+    let daysOfWeek = Date.capitalizedFirstLettersOfWeekdays
+    let coloums = Array(repeating: GridItem(.flexible()), count: 7)
+
     var body: some View {
         VStack {
-            LabeledContent("Calendar Color") {
-                ColorPicker("", selection: $color, supportsOpacity: false)
+            HStack {
+                ForEach(daysOfWeek.indices, id: \.self) { idx in
+                        Text(daysOfWeek[idx])
+                        .fontWeight(.black)
+                        .foregroundStyle(settingsService.calendarColor)
+                        .frame(maxWidth: .infinity)
+                }
             }
+            
+            LazyVGrid(columns: coloums, content: {
+                ForEach(days, id: \.self) { day in
+                    if day.monthInt != date.monthInt {
+                        Text("")
+                    } else {
+                        Text(day.formatted(.dateTime.day()))
+                            .fontWeight(.bold)
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(
+                                        Date.now.startOfDay == day.startOfDay
+                                        ? settingsService.currendDayColor.opacity(0.3)
+                                        : settingsService.calendarColor.opacity(0.3)
+                                    )
+                            )
+                    }
+                }
+            })
+        }
+        .padding()
+        .onAppear {
+            days = date.calendarDisplayDays
+        }
+        .onChange(of: date) {
+            days = date.calendarDisplayDays
         }
     }
 }
 
 #Preview {
-    CalendarView()
+    @StateObject var settingsService = SettingsService()
+    
+    return CalendarView(date: Date.now)
+            .environmentObject(settingsService)
 }
