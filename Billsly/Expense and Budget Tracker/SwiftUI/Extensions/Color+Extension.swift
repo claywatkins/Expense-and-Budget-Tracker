e19ac733432910118b85f450242bcc7937350618
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-22 10:36:03 -0700

    created ext to save color pref

--- Billsly/Expense and Budget Tracker/SwiftUI/Extensions/Color+Extension.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Extensions/Color+Extension.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color: RawRepresentable {
+
+    public init?(rawValue: String) {
+        guard let data = Data(base64Encoded: rawValue) else {
+            self = .blue
+            return
+        }
+        
+        do {
+            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .blue
+            self = Color(color)
+        } catch {
+            self = .blue
+        }
+    }
+
+    public var rawValue: String {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+        } catch {
+            return ""
+        }
+    }
+
+}
