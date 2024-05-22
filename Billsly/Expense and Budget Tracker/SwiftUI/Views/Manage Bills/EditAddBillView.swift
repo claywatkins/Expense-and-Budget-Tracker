d4de1ba4ef23d2c5d5b28ed81c092dd6d4f297c9
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-22 10:38:57 -0700

    created reusable view for edit/add bills

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/EditAddBillView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Manage Bills/EditAddBillView.swift
@@ -8,11 +8,121 @@
 import SwiftUI
 
 struct EditAddBillView: View {
+    @EnvironmentObject var userService: UserController
+    @Environment(\.dismiss) var dismiss
+    @State var isEdit: Bool
+    @State var bill: Bill?
+    @State private var billName: String = ""
+    @State private var billCost: String = ""
+    @State private var categorySelection: Category?
+    @State private var billDueDate: Date = Date.now
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Section {
+                        Text("Bill name")
+                            .foregroundStyle(.primary)
+                            .fontWeight(.semibold)
+                            .font(.title)
+                        
+                        TextField("Bill name", text: $billName)
+                            .foregroundStyle(.secondary)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Section {
+                        Text("Bill cost")
+                            .foregroundStyle(.primary)
+                            .fontWeight(.semibold)
+                            .font(.title)
+                        
+                        TextField("Bill cost", text: $billCost)
+                            .foregroundStyle(.secondary)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    Section {
+                        Text("Category")
+                            .foregroundStyle(.primary)
+                            .fontWeight(.semibold)
+                            .font(.title)
+                        
+                        Picker("", selection: $categorySelection) {
+                            Text(isEdit ? bill?.category.name ?? "" : "Choose a catagory").tag(nil as Category?)
+                            ForEach(userService.userCategories.indices, id: \.self) { idx in
+                                Text(userService.userCategories[idx].name).tag(userService.userCategories[idx] as Category?)
+                            }
+                        }
+                        .labelsHidden()
+                    }
+                    Section {
+                        Text("Due Date")
+                            .foregroundStyle(.primary)
+                            .fontWeight(.semibold)
+                            .font(.title)
+                        
+                        DatePicker("", selection: $billDueDate, displayedComponents: .date)
+                            .datePickerStyle(.graphical)
+                            .labelsHidden()
+                    }
+                    
+                    
+                    Button {
+                        // Save
+                        dismiss()
+                    } label: {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(Color("buttonColor", bundle: Bundle.main))
+                            .overlay {
+                                Text("Save Bill")
+                                    .foregroundStyle(Color(.label))
+                            }
+                            .frame(height: 50)
+                            .modifier(ShadowViewModifier())
+                    }
+                }
+                .padding()
+                .navigationTitle(isEdit ? "Edit Bill" : "Add a Bill")
+                .onAppear {
+                    if isEdit {
+                        if let bill = bill {
+                            billName = bill.name
+                            billCost = userService.currencyNf.string(from: bill.dollarAmount as NSNumber) ?? ""
+                            categorySelection = bill.category
+                            billDueDate = bill.dueByDate
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            HStack {
+                                Image(systemName: "chevron.backward")
+                                Text("Manage Bills")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
+
+
 #Preview {
-    EditAddBillView()
+    @StateObject var userService = UserController()
+    
+    return EditAddBillView(isEdit: true,
+                           bill: Bill(identifier: UUID().uuidString,
+                                      name: "Bill Name",
+                                      dollarAmount: 10.50,
+                                      dueByDate: Date.now,
+                                      category: .init(name: "Test Category"),
+                                      isOn30th: false,
+                                      hasImage: nil))
+    .environmentObject(userService)
 }
