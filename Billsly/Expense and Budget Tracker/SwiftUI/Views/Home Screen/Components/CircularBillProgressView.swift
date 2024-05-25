971f606284ff4b735728e23d614c045bc2c0d9de
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-24 18:55:34 -0700

    added new view to project

---
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/Components/CircularBillProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularBillProgressView.swift
+//  Expense and Budget Tracker
+//
+//  Created by Clayton Watkins on 5/22/24.
+//
+
+import SwiftUI
+
+struct CircularBillProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularBillProgressView()
+}
