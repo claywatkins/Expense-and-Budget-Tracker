1954cbbab1429b24c1e62f16b52dce01787ee289
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-07-30 15:50:22 -0700

    using bill service to update pay status

--- Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/Components/QuickPaidBillView.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Views/Home Screen/Components/QuickPaidBillView.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct QuickPaidBillView: View {
     @EnvironmentObject var userService: UserController
+    @EnvironmentObject var billService: BillService
     @Environment(\.dismiss) var dismiss
-    @State private var selectedBill: Bill?
+    @State private var selectedBill: NewBill?
     @Binding var counter: Int
+    
+    @Environment(\.modelContext) var context
+    
+    @Query(filter: #Predicate<NewBill> { bill in
+        bill.hasBeenPaid == false
+    }, sort: \NewBill.dueByDate, order: .forward) var unpaidBills: [NewBill]
     
     var body: some View {
         ScrollView {
@@ -21,16 +29,16 @@ struct QuickPaidBillView: View {
                     .foregroundStyle(.primary)
                 
                 Picker("What bill did you pay?", selection: $selectedBill) {
-                    Text("Choose a bill").tag(nil as Bill?)
-                    ForEach(userService.unpaidBills, id: \.self) { bill in
+                    Text("Choose a bill").tag(nil as NewBill?)
+                    ForEach(unpaidBills, id: \.self) { bill in
                         Text(bill.name)
-                            .tag(bill as Bill?)
+                            .tag(bill as NewBill?)
                     }
                 }
                 
                 Button {
                     if let selectedBill = selectedBill {
-                        userService.updateBillHasBeenPaid(bill: selectedBill)
+                        billService.markBillAsPaid(bill: selectedBill, context: context)
                         counter += 1
                     }
                     dismiss()
