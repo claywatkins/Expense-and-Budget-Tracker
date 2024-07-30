f0481cfd452006a72adc43ceec7dc36033725e5a
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-07-30 10:55:58 -0700

    re added codable, added support for codable

--- Billsly/Expense and Budget Tracker/SwiftUI/Model/Bill.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Model/Bill.swift
@@ -9,13 +9,13 @@ import Foundation
 import SwiftData
 
 @Model
-struct Bill: Equatable, Hashable {
+class Bill: Codable, Equatable, Hashable {
     var identifier: String
     var name: String
     var dollarAmount: Double
     var dueByDate: Date
     var hasBeenPaid: Bool
-    var category: Category
+    var category: String
     var isOn30th: Bool
     var hasImage: Bool?
     
@@ -23,7 +23,7 @@ struct Bill: Equatable, Hashable {
          name: String,
          dollarAmount: Double,
          dueByDate: Date,
-         category: Category,
+         category: String,
          isOn30th: Bool,
          hasImage: Bool?) {
         self.identifier = identifier
@@ -35,8 +35,47 @@ struct Bill: Equatable, Hashable {
         self.isOn30th = isOn30th
         self.hasImage = hasImage
     }
-    
+        
+    static func == (lhs: Bill, rhs: Bill) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+        
     func hash(into hasher: inout Hasher) {
         hasher.combine(identifier)
+    }
+        
+    enum CodingKeys: String, CodingKey {
+        case identifier
+        case name
+        case dollarAmount
+        case dueByDate
+        case hasBeenPaid
+        case category
+        case isOn30th
+        case hasImage
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.identifier = try container.decode(String.self, forKey: .identifier)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.dollarAmount = try container.decode(Double.self, forKey: .dollarAmount)
+        self.dueByDate = try container.decode(Date.self, forKey: .dueByDate)
+        self.hasBeenPaid = try container.decode(Bool.self, forKey: .hasBeenPaid)
+        self.category = try container.decode(String.self, forKey: .category)
+        self.isOn30th = try container.decode(Bool.self, forKey: .isOn30th)
+        self.hasImage = try container.decodeIfPresent(Bool.self, forKey: .hasImage)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(identifier, forKey: .identifier)
+        try container.encode(name, forKey: .name)
+        try container.encode(dollarAmount, forKey: .dollarAmount)
+        try container.encode(dueByDate, forKey: .dueByDate)
+        try container.encode(hasBeenPaid, forKey: .hasBeenPaid)
+        try container.encode(category, forKey: .category)
+        try container.encode(isOn30th, forKey: .isOn30th)
+        try container.encode(hasImage, forKey: .hasImage)
     }
 }
--- Billsly/Expense and Budget Tracker/SwiftUI/Model/Category.swift
+++ Billsly/Expense and Budget Tracker/SwiftUI/Model/Category.swift
@@ -9,10 +9,33 @@ import UIKit
 import SwiftData
 
 @Model
-struct Category: Equatable, Hashable {
+class Category: Codable, Equatable, Hashable {
     var name: String
     
     init(name: String) {
         self.name = name
     }
+    
+    static func == (lhs: Category, rhs: Category) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
 }
+
